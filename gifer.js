8340bfb4a337a2afcabf06ef8d34d6643005b2dd
Mikkkee <boy.swordman@gmail.com>
2016-04-05 23:24:18 +0800

    Format js html css files

--- gifer.js
+++ gifer.js
@@ -1,38 +1,39 @@
 'use strict';
 
-function hold() {
+function init() {
 
-  const holder = document.getElementById('holder');
-  const dragText = document.getElementById('drag-text');
-  holder.ondragover = function () {
-    return false;
-  };
+    // Drag and Drop holder.
+    const holder = document.getElementById('holder');
+    const dragText = document.getElementById('drag-text');
 
-  holder.ondragleave = holder.ondragend = function () {
-    return false;
-  };
+    holder.ondragover = function() {
+        return false; };
 
-  holder.ondrop = function (e) {
-    e.preventDefault();
-    const file = e.dataTransfer.files[0];
-    console.log('File you dragged here is', file.path);
+    holder.ondragleave = holder.ondragend = function() {
+        return false;
+    };
 
-    // Remove exitng video.
-    const existingVideo = holder.getElementsByTagName('video')[0];
-    if (existingVideo) { existingVideo.remove(); };
-    
-    dragText.className += ' hidden';
+    holder.ondrop = function(e) {
+        e.preventDefault();
+        const file = e.dataTransfer.files[0];
+        console.log('File you dragged here is', file.path);
 
-    const video = document.createElement("video");
-    video.setAttribute('controls', '');
-    video.setAttribute("width", '100%');
-    video.setAttribute('height', '100%');
-    const source = document.createElement("source");
-    source.setAttribute('src', file.path);
-    video.appendChild(source);
-    holder.appendChild(video);
-    return false;
-  };
+        // Remove exitng video.
+        const existingVideo = holder.getElementsByTagName('video')[0];
+        if (existingVideo) { existingVideo.remove(); };
+
+        dragText.className += ' hidden';
+
+        const video = document.createElement("video");
+        video.setAttribute('controls', '');
+        video.setAttribute("width", '100%');
+        video.setAttribute('height', '100%');
+        const source = document.createElement("source");
+        source.setAttribute('src', file.path);
+        video.appendChild(source);
+        holder.appendChild(video);
+        return false;
+    };
 };
 
-window.onload = hold;
+window.onload = init;
--- main.js
+++ main.js
@@ -10,21 +10,21 @@ const BrowserWindow = electron.BrowserWindow;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1080, height: 760});
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({ width: 1080, height: 760 });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.webContents.openDevTools();
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 }
 
 // This method will be called when Electron has finished
@@ -32,18 +32,18 @@ function createWindow () {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('activate', function() {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
