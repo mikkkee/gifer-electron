46c5e68c9e53b393c642c97fce7441dc5fbd47dc
mikkkee <boy.swordman@gmail.com>
2016-04-05 22:18:09 +0800

    Drag and drop movie files into DOM

--- gifer.js
+++ gifer.js
@@ -1,17 +1,36 @@
 'use strict';
 
 function hold() {
-  var holder = document.getElementById('holder');
+
+  const holder = document.getElementById('holder');
+  const dragText = document.getElementById('drag-text');
   holder.ondragover = function () {
     return false;
   };
+
   holder.ondragleave = holder.ondragend = function () {
     return false;
   };
+
   holder.ondrop = function (e) {
     e.preventDefault();
-    var file = e.dataTransfer.files[0];
+    const file = e.dataTransfer.files[0];
     console.log('File you dragged here is', file.path);
+
+    // Remove exitng video.
+    const existingVideo = holder.getElementsByTagName('video')[0];
+    if (existingVideo) { existingVideo.remove(); };
+    
+    dragText.className += ' hidden';
+
+    const video = document.createElement("video");
+    video.setAttribute('controls', '');
+    video.setAttribute("width", '100%');
+    video.setAttribute('height', '100%');
+    const source = document.createElement("source");
+    source.setAttribute('src', file.path);
+    video.appendChild(source);
+    holder.appendChild(video);
     return false;
   };
 };
