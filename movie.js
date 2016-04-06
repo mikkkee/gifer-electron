4efe1a70409739518312ce5f3d7c5bf10d6c99f1
Mikkkee <boy.swordman@gmail.com>
2016-04-06 23:37:47 +0800

    Change style: use 2 spaces indentation

--- movie.js
+++ movie.js
@@ -1,37 +1,37 @@
 const videoClip = {
-      video: '',
-      initWidth: 0,
-      initHeight: 0,
-      width: 0,
-      height: 0,
-      start: 0;
-      end: 0;
-      duration: function () {
-        return this.end - this.start;
-      };
-      mirror: false,
-      scale: 1.0,
-      fps: 0.0,
-      speed: 1.0,
-      ratio: 0.0,    // initWidth / initHeight.
-      setWidth: function (w) {
-        // Set width to w and keep aspect ratio unchanged.
-        this.width = w;
-        this.height = this.width / this.ratio;
-        return this;
-      },
-      setHeight: function (h) {
-        // Set Height to h and keep aspect ratio unchanged.
-        this.height = h;
-        this.weight = this.height * this.ratio;
-        return this;
-      },
-      setScale: function (s) {
-        this.scale = s;
-        this.width = this.initWidth * this.scale;
-        this.height = this.initHeight * this.scale;
-        return this;
-      }
-    };
+  video: '',
+  initWidth: 0,
+  initHeight: 0,
+  width: 0,
+  height: 0,
+  start: 0;
+  end: 0;
+  duration: function() {
+    return this.end - this.start;
+  };
+  mirror: false,
+  scale: 1.0,
+  fps: 0.0,
+  speed: 1.0,
+  ratio: 0.0, // initWidth / initHeight.
+  setWidth: function(w) {
+    // Set width to w and keep aspect ratio unchanged.
+    this.width = w;
+    this.height = this.width / this.ratio;
+    return this;
+  },
+  setHeight: function(h) {
+    // Set Height to h and keep aspect ratio unchanged.
+    this.height = h;
+    this.weight = this.height * this.ratio;
+    return this;
+  },
+  setScale: function(s) {
+    this.scale = s;
+    this.width = this.initWidth * this.scale;
+    this.height = this.initHeight * this.scale;
+    return this;
+  }
+};
 
 module.exports = videoClip;
