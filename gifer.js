36c799b506c547743f47b212747510923e135f5d
mikkkee <boy.swordman@gmail.com>
2016-04-11 20:34:31 +0800

    Bind events on options

--- gifer.js
+++ gifer.js
@@ -1,64 +1,132 @@
 'use strict';
 
 const videoClip = require('./movie.js');
+const remote = require('electron').remote;
+const Menu = require('electron').remote.Menu;
+const MenuItem = require('electron').remote.MenuItem;
 let video;
 
-function init() {
+function Init() {
   video = document.querySelector('video');
-  bindEvents();
-  const abc = new Bind({clip: videoClip}, {
-    'clip.video': 'input#video-input',
-    'clip.start': 'input#start-input',
-    'clip.end': 'input#end-input',
-    'clip.width': 'input#width-input',
-    'clip.height': 'input#height-input',
-    'clip.scale': 'input#scale-input',
-    'clip.fps': 'input#fps-input',
-    'clip.speed': 'input#speed-input'});
+  BindEvents();
 }
 
-function bindEvents() {
+function BindEvents() {
   // Drag and Drop holder.
   const holder = document.getElementById('holder');
   // Placehold text in holder.
   const dragText = document.getElementById('drag-text');
+  const doScale = document.querySelector('#do-scale');
+  const makeBtn = document.querySelector('#make-gif');
+  const resetBtn = document.querySelector('#reset');
+  const gvSwitch = document.querySelector('#gif-video-switch');
   // Disable drag and drop event for document.
-  document.addEventListener('dragover', idle, false);
-  document.addEventListener('drop', idle, false);
+  document.addEventListener('dragover', Idle, false);
+  document.addEventListener('drop', Idle, false);
 
-  holder.ondragover = idle;
-  holder.ondragleave = holder.ondragend = idle;
-  holder.ondrop = loadVideo;
+  holder.ondragover = Idle;
+  holder.ondragleave = holder.ondragend = Idle;
+  holder.ondrop = LoadVideoFromDrop;
+
+  doScale.onchange = ToggleScale;
+
+  makeBtn.addEventListener('click', function(){videoClip.MakeGIF();});
+  resetBtn.addEventListener('click', Reset);
+  gvSwitch.addEventListener('click', function(){ToggleGV(event);});
+
+  const bind = new Bind({clip: videoClip}, {
+    'clip.video': 'input#video-input',
+    'clip.start': {
+      dom: 'input#start-input',
+      callback: CalDuration,
+    },
+    'clip.end': {
+      dom: 'input#end-input',
+      callback: CalDuration,
+    },
+    'clip.width': 'input#width-input',
+    'clip.height': 'input#height-input',
+    'clip.scale': {
+      dom: 'input#scale-input',
+      callback: videoClip.UpdateWithScale,
+    },
+    'clip.fps': 'input#fps-input',
+    'clip.speed': 'input#speed-input',
+    'clip.mirror': 'input#do-mirror'
+    });
 }
 
-function idle(e) {
+function ToggleGV(event) {
+  const video = document.querySelector('video');
+  const gif = document.querySelector('#gif');
+  const dragText = document.getElementById('drag-text');
+  if (event.target.checked) {
+    video.classList.remove('hidden');
+    gif.className += ' hidden';
+    dragText.className += ' hidden';
+  } else {
+    video.className += ' hidden';
+    gif.classList.remove('hidden');
+    dragText.className += ' hidden';
+  }
+}
+
+function ToggleScale() {
+  const doScale = document.querySelector('#do-scale');
+  const scaleInput = document.querySelector('input#scale-input');
+  const heightInput = document.querySelector('input#height-input');
+  const widthInput = document.querySelector('input#width-input');
+  if (doScale.checked) {
+    scaleInput.removeAttribute('disabled');
+    heightInput.setAttribute('disabled', '');
+    widthInput.setAttribute('disabled', '');
+  } else {
+    scaleInput.setAttribute('disabled', '');
+    heightInput.removeAttribute('disabled');
+    widthInput.removeAttribute('disabled');
+  }
+}
+
+function CalDuration() {
+  const duration = document.querySelector('input#duration-input');
+  duration.value = videoClip.Duration();
+}
+
+function Idle(e) {
   e.preventDefault();
   return false;
 }
 
-function videoError(msg) {
+function VideoError(msg) {
   console.log(msg);
 }
 
-function loadVideo(e) {
+function LoadVideo(videoFile) {
+  videoClip.LoadVideo(videoFile);
+}
+
+function LoadVideoFromDrop(e) {
   e.preventDefault();
 
   const dragText = document.getElementById('drag-text');
   const file = e.dataTransfer.files[0];
 
-  console.log('File you dragged here is ', file.path);
-  videoClip.loadVideo(file.path);
+  videoClip.LoadVideo(file.path);
 
   video.src = file.path;
   dragText.className += ' hidden';
   video.classList.remove('hidden');
   setTimeout(function() {
-    if (!video.videoWidth) videoError('Cannot play video');
+    if (!video.videoWidth) VideoError('Cannot play video');
   }, 200);
 }
 
-function reset() {
+function Reset() {
   // Reset options to values of video file.
+  const doScale = document.querySelector('input#do-scale');
+  doScale.checked = false;
+  ToggleScale();
+  videoClip.Reset();
 }
 
-window.onload = init;
+window.onload = Init;
--- movie.js
+++ movie.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const os = require('os');
+const dialog = require('electron').remote.dialog;
 const exec = require('child_process').exec;
 
 const videoClip = {
@@ -11,57 +12,110 @@ const videoClip = {
   height: 0,
   start: 0,
   end: 0,
-  duration: function() {
+  initEnd: 0,
+  Duration: function() {
     return this.end - this.start;
   },
   mirror: false,
-  scale: 1.0,
+  scale: '',
   fps: 0.0,
+  initFps: 0.0,
   speed: 1.0,
   ratio: 0.0, // initWidth / initHeight.
+  initRatio: 0.0,
   platform: os.platform(),
-  loadVideo: function (path) {
+  ffprobe: os.platform() === 'win32' ? '.\\bin\\ffprobe.exe' : './bin/ffprobe',
+  ffmpeg: os.platform() === 'win32' ? '.\\bin\\ffmpeg.exe' : './bin/ffmpeg',
+  LoadVideo: function (path) {
     this.video = path;
-    this.getVideoInfo();
+    this.GetVideoInfo();
   },
-  getVideoInfo: function(){
+  GetVideoInfo: function(){
     // Get detailed video info by calling ffprobe.
     const _this = this;
-    let ffprobe = './bin/ffprobe';
-    if (this.platform === 'win32') ffprobe = '.\\bin\\ffprobe.exe';
-    exec(ffprobe + ' -v quiet -print_format json -show_format -show_streams ' + this.video,
+
+    exec(this.ffprobe + ' -v quiet -print_format json -show_format -show_streams ' + this.video,
       function callback(error, stdout, stderr){
         if (error) {
+          alert("Please select a video file.");
           console.log(error);
         } else {
           const videoStream = JSON.parse(stdout).streams[0];
           _this.width = videoStream.width;
           _this.height = videoStream.height;
-          _this.start = videoStream.start_time;
-          _this.end = videoStream.duration;
+          _this.initWidth = videoStream.width;
+          _this.initHeight = videoStream.height;
+          _this.ratio = _this.initWidth / _this.initHeight;
+          _this.initRatio = _this.initWidth / _this.initHeight;
+          _this.start = eval(videoStream.start_time).toFixed(3);
+          _this.end = eval(videoStream.duration).toFixed(3);
+          _this.initEnd = _this.end;
           _this.fps = eval(videoStream.r_frame_rate).toFixed(2);
+          _this.initFps = _this.fps;
         }
         return;
       });
   },
-  setWidth: function(w) {
+  SetWidth: function(w) {
     // Set width to w and keep aspect ratio unchanged.
     this.width = w;
     this.height = this.width / this.ratio;
     return this;
   },
-  setHeight: function(h) {
+  SetHeight: function(h) {
     // Set Height to h and keep aspect ratio unchanged.
     this.height = h;
     this.weight = this.height * this.ratio;
     return this;
   },
-  setScale: function(s) {
+  SetScale: function(s) {
     this.scale = s;
     this.width = this.initWidth * this.scale;
     this.height = this.initHeight * this.scale;
     return this;
-  }
+  },
+  UpdateWithScale: function(){
+    if (this.scale) {
+      this.width = this.initWidth * this.scale;
+      this.height = this.initHeight * this.scale;
+    }
+    return this;
+  },
+  Reset: function(){
+    this.height = this.initHeight;
+    this.width = this.initWidth;
+    this.end = this.initEnd;
+    this.ratio = this.initRatio;
+    this.fps = this.initFps;
+    this.mirror = false;
+    this.scale = '';
+    this.speed = 1;
+  },
+  MakeGIF: function(callback) {
+    // ffmpeg
+    // -ss [start] -t [duration]
+    // -i [input video]
+    // -vf fps=[fps],scale=[width]:[height]:flags=lanczos,setpts=1/[speed]*PTS
+    // output.gif
+    const _this = this;
+    dialog.showSaveDialog({
+      title: 'hehe',
+      defaultPath: 'C:\\',
+      filters: [
+      {name: 'GIFs', extensions: ['gif']},
+      {name: 'All Files', extensions: ['*']}
+      ],
+    }, function(gifname) {
+      const options = ' -ss ' + _this.start + ' -t ' + _this.Duration()
+        + ' -i ' + _this.video + ' -vf ' + 'fps=' + _this.fps + ',scale='
+        + _this.width + ':' + _this.height + ':flags=lanczos,'
+        + 'setpts=1/' + _this.speed + '*PTS ' + gifname;
+      exec(_this.ffmpeg + options, function (error, stdout, stderr) {
+        console.log('GIF convert finished!');
+        callback();
+      });
+    });
+  },
 };
 
 module.exports = videoClip;
