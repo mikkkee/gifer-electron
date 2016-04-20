03f91f58d92a5c577f920d824ec0c205d396f011
Mikkkee <boy.swordman@gmail.com>
2016-04-20 09:53:47 +0800

    Add package cmd

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-electron-packager . gifer-electron --platform=darwin --arch=x64 --out=./build --ignore=./build --overwrite --icon=resources/logo_icon.ico
+electron-packager . gifer-electron --platform=darwin --arch=x64 --out=./build --ignore=./build --overwrite --icon=./resources/logo_icon.ico
